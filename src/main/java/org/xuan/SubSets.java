4f9121c9ba522fbc5409a11d9e02e61dc6949fed
reformasky <reformasky01@gmail.com>
2015-12-21 21:51:39 -0800

    78 revised

--- src/main/java/org/xuan/SubSets.java
+++ src/main/java/org/xuan/SubSets.java
@@ -17,20 +17,12 @@ public class SubSets {
         }
         Arrays.sort(nums);
         Integer[] cache = new Integer[nums.length];
-        dfs(nums, 0, 0, cache, result);
+        dfs(nums, 0, -1, cache, result);
         return result;
     }
 
     private void dfs(int[] nums, int index, int lastIndex, Integer[] cache, List<List<Integer>> result) {
-        if (index == 0) {
-            for(int i = 0; i < nums.length; i++) {
-                cache[index] = nums[i];
-                Integer[] temp =  Arrays.copyOfRange(cache, 0, index+1);
-                result.add(new ArrayList<>(Arrays.asList(temp)));
-                dfs(nums, index + 1, i,  cache, result);
-            }
-        }
-        else if (index == cache.length) {
+        if (index == cache.length) {
             return;
         }
         else {
